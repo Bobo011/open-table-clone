e8dcf494c31d50713b03935c9a84801238d71018
George <alximik15@gmail.com>
2023-11-12 00:28:25 +0400

    Changed Search page Title

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import Header from '../components/Header';
-import RestaurantNavbar from '../components/RestaurantNavbar';
-import  Menu  from '../components/Menu';
+import React from "react";
+import Header from "../components/Header";
+import RestaurantNavbar from "../components/RestaurantNavbar";
+import Menu from "../components/Menu";
 
 const RestaurantMenu = () => {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[100%] rounded p-3 shadow">
-          <RestaurantNavbar />
-          <Menu />
-        </div>
+      <div className="bg-white w-[100%] rounded p-3 shadow">
+        <RestaurantNavbar />
+        <Menu />
       </div>
     </>
   );
-}
+};
 
-export default RestaurantMenu
+export default RestaurantMenu;
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,23 +1,29 @@
 import NavBar from "@/app/components/NavBar";
 import React from "react";
-import {Header,RestaurantNavbar,Title,Rating,Description,Images,Reviews,ReservationCard} from './components/index'
+import {
+  Header,
+  RestaurantNavbar,
+  Title,
+  Rating,
+  Description,
+  Images,
+  Reviews,
+  ReservationCard,
+} from "./components/index";
 
 const RestaurantDetails = () => {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <RestaurantNavbar />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <div className="w-[27%] relative text-reg">
-          <ReservationCard />
-        </div>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavbar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
       </div>
     </>
   );
