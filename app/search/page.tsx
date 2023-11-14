0915b7375858e0030942c31bb72e509917555e30
George <alximik15@gmail.com>
2023-11-14 22:38:38 +0400

    R

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/Header";
+import Header from "../components/Header";
 import SearchSidebar from "./components/SearchSidebar";
 import RestaurantCard from "./components/RestaurantCard";
 import { Metadata } from "next";
