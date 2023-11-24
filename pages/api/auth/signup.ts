7cc3afe335ee660adf313034e968eda44d3c5789
George <alximik15@gmail.com>
2023-11-24 19:58:17 +0400

    Sign Up is done

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -3,6 +3,7 @@ import {NextApiRequest,NextApiResponse} from 'next'
 import validator from 'validator'
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt'
+import * as jose from "jose";
 
 
 const prisma = new PrismaClient
@@ -84,9 +85,17 @@ const hashedPassword = await  bcrypt.hash(password,10)
    },
  });
 
+ const alg = 'HS256'
+ const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+const token = await new jose.SignJWT({ email: user.email })
+  .setProtectedHeader({ alg })
+  .setExpirationTime("24h")
+  .sign(secret);
+
 	res.status(200).json({
-		hello:user
-	})
+    hello: token,
+  });
 }
 
 }
