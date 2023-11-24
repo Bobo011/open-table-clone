32fcd0a06f52b23fbff381dc7f1a86e2587721dc
George <alximik15@gmail.com>
2023-11-24 18:19:49 +0400

    Validation schema is done

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,65 @@
+
+import {NextApiRequest,NextApiResponse} from 'next'
+import validator from 'validator'
+
+
+export default async function handler(
+	req:NextApiRequest,
+	res:NextApiResponse
+	)
+	{
+
+if(req.method === 'POST'){
+
+const { firstName, lastName,email,phone,city,password } = req.body;
+const errors:string[] = []
+
+
+const validationsSchema = [
+  {
+    valid: validator.isLength(firstName, {
+      min: 1,
+      max: 20,
+    }),
+    errorMessage: "First name is Invalid",
+  },
+  {
+    valid: validator.isLength(lastName, {
+      min: 1,
+      max: 20,
+    }),
+    errorMessage: "Last name is Invalid",
+  },
+  {
+    valid: validator.isEmail(email),
+    errorMessage: "Email is Invalid",
+  },
+  {
+    valid: validator.isMobilePhone(phone),
+    errorMessage: "Phone number is Invalid",
+  },
+  {
+    valid: validator.isLength(city,{min:1}),
+    errorMessage: "City Name is Invalid",
+  },
+  {
+    valid: validator.isStrongPassword(password),
+    errorMessage: "Password Is not strong enough",
+  },
+];
+validationsSchema.forEach((check)=>{
+	  if(!check.valid){
+errors.push(check.errorMessage)
+	  }
+})
+
+if(errors.length){
+	return res.status(400).json({errorMessage:errors[0]})
+}
+
+	res.status(200).json({
+		hello:"body"
+	})
+}
+
+}
