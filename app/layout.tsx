049a344e3d50c49ba94ce514fa9015054bd3f6cb
George <alximik15@gmail.com>
2023-11-29 18:12:44 +0400

    Time and Dates for Reservation are set

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import AuthContext from "./context/AuthContext";
+import 'react-datepicker/dist/react-datepicker.css'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
