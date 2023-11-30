9265296edfa4db577359e56bd7823f5cde3cf9bf
George <alximik15@gmail.com>
2023-11-30 19:06:16 +0400

    Now i can fetch booking table objcts

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,4 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { times } from "../../../../data";
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient()
+
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,7 +23,45 @@ if(!day || !time || !partySize){
 	})
 }
 
-return res.json({slug,day,time,partySize})
+const searchTimes = times.find(t => {
+  return t.time === time
+})?.searchTimes
+
+if(!searchTimes){
+  	return res.status(400).json({
+      errorMessage: "Invalid data provided",
+    });
+}
+
+const bookings = await prisma.booking.findMany({
+  where: {
+    booking_time: {
+      gte: new Date(`${day}T${searchTimes[0]}`),
+      lte: new Date(`${day}T${searchTimes[searchTimes.length-1]}`),
+    },
+  },
+  select:{
+    number_of_people:true,
+    booking_time:true,
+    tables:true
+  }
+});
+
+const bookingTablesObj:{[key:string]:{[key:number]:true}} = {}
+
+
+bookings.forEach(booking => {
+  bookingTablesObj[booking.booking_time.toISOString()]= booking.tables.reduce((obj,table)=>{
+    return {
+      ...obj,
+      [table.table_id] : true
+    }
+
+  },{})
+});
+
+return res.json({ searchTimes, bookings, bookingTablesObj });
 
 }
 
+//http://localhost:3000/api/restaurant/last-train-to-delhi-ottawa/availability?day=2023-05-27&time=14:00:00.000Z&partySize=4
